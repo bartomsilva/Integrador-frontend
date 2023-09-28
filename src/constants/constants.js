e29cd02484acd283ef80363f1304f689c9d8d5c7
bartomsilva <bartomsilva@gmail.com>
2023-09-28 18:37:25 -0300

    finalizado api rodando na aws

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,5 @@
 
-  // export const BASE_URL = "https://labeddit-back-end-mku5.onrender.com"
-// export const BASE_URL = "http://34.227.115.117:3003"
-export const BASE_URL = "http://127.0.0.1:3003"
+// export const BASE_URL = "https://labeddit-back-end-mku5.onrender.com"
+export const BASE_URL = "http://18.204.122.118:3003"
+// export const BASE_URL = "http://127.0.0.1:3003"
 
